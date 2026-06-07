df8ee5b4b890f480ab9dec6e4c1104316d33b036
cfcitprogrammer-cloud <cfcit@gmail.com>
2026-06-08 06:17:54 +0800

    updated code

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,2 +1,2 @@
 export const WEBAPP_GAS_URL =
-  "https://script.google.com/macros/s/AKfycbzwhXYMSC-BRtFdP66oso89_-RzfI2irYvetTBm1aO2mqTeTY-Q9rRO0s4dDDNQRV3K/exec";
+  "https://script.google.com/macros/s/AKfycbzkAbRxpZZoY5X7xImVinOKqpq96uLjkmMqnIrDIoiCGsZj37JIFm7ED18VYRmiLSVL/exec";
